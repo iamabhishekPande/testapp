9eac02eb8c9db6728027c3c0b9a5099572f98821
iamabhishekPande <imabhishekpandey1196@gmail.com>
2023-12-13 19:15:53 +0530

    first commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 8081;
 
 // Serve index.html as the root route
 app.get('/', (req, res) => {
